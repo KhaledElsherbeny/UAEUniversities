d3038435b0db9017aee14c6d7c98f83fc755e67a
Khalid <khaledelsherbeny900@gmail.com>
2024-05-05 04:11:36 +0300

    adding missing domain module

---
+++ Modules/Domain/Sources/Domain/UniversityListItem.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+public struct UniversityListItem {
+    public var alphaTwoCode: String = ""
+    public var name: String = ""
+    public var country: String = ""
+    public var domains: [String] = []
+    public var webPages: [String] = []
+    public var stateProvince: String?
+    
+    public init() {
+        
+    }
+}
